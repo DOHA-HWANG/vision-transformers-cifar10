ac6bf813e69e95c923c9b15b7841af20a17c1ad5
DOHA-HWANG <ehgkehgk.ca@gmail.com>
2022-03-14 16:32:12 -0400

    Fixed normalization for dataset
    Have to use same 'normalize' values between train and test

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ def my_Cifar10(imageSize=224, aug=False):
                                     transforms.RandomHorizontalFlip(),
                                     transforms.RandomRotation(15),
                                     transforms.ToTensor(),
-                                    transforms.Normalize(0.5, 0.5)
+                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                     ])
 
     transform_test = transforms.Compose(
